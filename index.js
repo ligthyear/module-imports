f40a15323a16b1d5afb9eee7022006f814e64ba4
Benjamin Kampmann <ben.kampmann@gmail.com>
2015-09-12 12:55:13 +0200

    export for all in list, too

--- index.js
+++ index.js
@@ -10,16 +10,16 @@ module.exports = function(source) {
       ext = query.ext || "/module.jsx",
       mods = JSON.parse(source)[key];
 
-  function make_string(x) { return 'require("' + prefix + x + ext + '")'}
+  function make_string(x) { return 'require("' + prefix + x + ext + '");'}
 
   if (!mods) throw "Nothing to import found in configuration!";
 
 
   if (!Array.isArray(mods)){
-    return 'exports["default"] = ' + make_string(mods) + ';';
+    return 'exports["default"] = ' + make_string(mods);
   }
 
   return mods.map(function(x){
-    return make_string(x) + ';';
+    return 'exports["' + x + '"] = ' + make_string(x);
   }).join("\n");
 };
