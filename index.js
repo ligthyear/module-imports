31dce1365bfe56788e9a805288c18b33968e6cae
Benjamin Kampmann <ben.kampmann@gmail.com>
2015-09-12 12:12:49 +0200

    New Version: 1.2  - now with more options to configure the imports  - support for non-list item imports

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 var path = require("path");
+var utils = require('loader-utils');
 
 module.exports = function(source) {
   this.cacheable();
-  var mods = JSON.parse(source).MODULES;
-  if (!mods) throw "No MODULES found in configuration";
 
-  var prefix = this.query ? this.query.slice(1) : "./modules/"
+  var query = utils.parseQuery(this.query),
+      key = query.key || "MODULES",
+      prefix = query.path || "./modules/",
+      ext = query.ext || "/module.jsx",
+      mods = JSON.parse(source)[key];
+
+  if (!mods) throw "Nothing to import found in configuration!";
+  if (!Array.isArray(mods)) mods = [mods];
+
   return mods.map(function(x){
-    return 'require("' + prefix + x + '/module.jsx");';
+    return 'require("' + prefix + x + ext + '");';
   }).join("\n");
 };
